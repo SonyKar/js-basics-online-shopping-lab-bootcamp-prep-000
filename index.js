791787690799e9b56a831c514ba3530df3efedd7
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 17:17:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  var tmp = {
    itemName: item,
-   itemPrice: Math.Floor(Math.Random() * (101 - 1) + 1)
+   itemPrice: Math.Floor(Math.Random() * (101 - 1 + 1)
  };
 }
 
