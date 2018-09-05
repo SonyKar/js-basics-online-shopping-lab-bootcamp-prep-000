ccbe5252a001669047cfdcb11e00c0bef5883696
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 17:38:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === "") {
+  if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var sum = total();
