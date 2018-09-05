63e68c33037b341444b1f319a4cc761e29cbef75
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 17:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   
   if (cart.length === 0) return "Your shopping cart is empty.";
     else if (cart.length === 1) {
-      
+      tmp += cart[i].itemName + " at $" + cart[i].itemPrice + ".";
     } else {
         while(i < cart.length) {
           if (i === cart.length - 1) tmp += " and " + cart[i].itemName + " at $" + cart[i].itemPrice + ".";
