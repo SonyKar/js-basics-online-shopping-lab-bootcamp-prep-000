63deb2ee428483a95e452c658e9d8023206e3796
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 17:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
   }
   
   if (flag === false) {
-    return "That item is not in your cart";
+    return "That item is not in your cart.";
   } else return cart;
 }
 
